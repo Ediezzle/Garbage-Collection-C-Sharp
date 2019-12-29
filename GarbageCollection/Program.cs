3fef5ff2eb9c4c059ce20a3de1d0415988e16773
Ediezzle <edmore.mandikiyana@gmail.com>
2019-12-29 18:56:58 +0200

    once off commit

---
+++ GarbageCollection/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GarbageCollection
+{
+    class User
+    {
+        public User()
+        {
+            Console.WriteLine("An Instance of class created");
+        }
+
+        // Destructor
+        ~User()
+        {
+            Console.WriteLine("An Instance of class destroyed");
+        }
+        static void Main(string[] args)
+        {
+            Details();
+            GC.Collect();
+            Console.ReadKey();
+        }
+
+        public static void Details()
+        {
+            // Created instance of class
+            User user = new User();
+        }
+    }
+}
